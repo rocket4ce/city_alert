972b21b42867a4b4df4d35a11afab373c2fe48ee
Rocket <rocket4ce@gmail.com>
2018-07-10 05:22:20 -0400

    mapa

--- Gemfile
+++ Gemfile
@@ -38,6 +38,7 @@ gem 'devise'
 gem 'cancan' # or cancancan
 gem 'draper'
 gem 'pundit'
+gem 'activeadmin_latlng'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
--- app/admin/alert.rb
+++ app/admin/alert.rb
@@ -11,5 +11,15 @@ ActiveAdmin.register Alert do
 #   permitted << :other if params[:action] == 'create' && current_user.admin?
 #   permitted
 # end
+  form do |f|
+    f.inputs do
+
+      f.input :latitude
+      f.input :longitude
+      f.input :imagen, :as => :file, :hint => image_tag(f.object.imagen.url)
+      f.latlng lang: :ru, map: :google, height: 500, loading_map: false, api_key_env: 'AIzaSyBuyw4RZd7ORJjyPtCfLBH3kzThgp3eYqw'
+    end
+    f.actions
+  end
 
 end
